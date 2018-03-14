53528346aceca300775af15b37f5a11e2b6b288e
AlvaroLopez-Jurado <alvaro17lj@gmail.com>
2018-03-14 09:44:35 -0700

    Test alvaro

--- tests/test_strings_counter.py
+++ tests/test_strings_counter.py
@@ -6,16 +6,15 @@ from sample.strings_counter import StringsCounter
 class TestStringsCounter(unittest.TestCase):
 
     def test_count_strings(self):
-        text = ("verano verano verano primavera primavera primavera "
-                "invierno invierno invierno")
-        solution = [('verano', 3), ('primavera',3), ('invierno', 3)]
+        text = ("verano verano verano")
+        solution = [('verano', 3)]
 
         result, words, set_words = StringsCounter.count_strings(text)
 
         assert collections.Counter(result) == collections.Counter(solution)
 
 
-    def test_meter_numeros(self):
+    def test_meter_numeros_String(self):
         text = ("1 2 3 1 2 3 1 2 3 4 alvaro 6")
 
         solution = [('1', 3), ('2', 3), ('3', 3), ('4', 1), ('alvaro', 1), ('6', 1)]
@@ -23,6 +22,17 @@ class TestStringsCounter(unittest.TestCase):
         result, words, set_words = StringsCounter.count_strings(text)
 
         assert collections.Counter(result) == collections.Counter(solution)
+
+
+    def test_count_varios_Strings(self):
+        text = ("verano verano verano primavera primavera primavera "
+                "invierno invierno invierno")
+        solution = [('verano', 3), ('primavera',3), ('invierno', 3)]
+
+        result, words, set_words = StringsCounter.count_strings(text)
+
+        assert collections.Counter(result) == collections.Counter(solution)
+        
 
 if __name__ == '__main__':
     unittest.main()
