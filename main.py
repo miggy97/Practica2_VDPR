8cae8ef8857fc6dce5da12b157408d919e7c5930
Miguel Ingelmo <miguelingelmo@hotmail.com>
2018-03-14 16:57:49 +0100

    Strings_counter ahora solo devuelve un dic

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from sample.strings_counter import StringsCounter
 if __name__ == "__main__":
     args = sys.argv[1:]
     #Esto imprimira el resultado de forma bonita
-    dic, words, set_words = StringsCounter.count_strings(args)
-    StringsCounter.print_solution(dic, words, set_words)
+    dic = StringsCounter.count_strings(args)
+    StringsCounter.print_solution(dic)
--- tests/test_strings_counter.py
+++ tests/test_strings_counter.py
@@ -8,13 +8,12 @@ class TestStringsCounter(unittest.TestCase):
     def test_count_strings(self):
         text = ("verano verano verano primavera primavera primavera "
                 "invierno invierno invierno")
-        solution = [('verano', 3), ('primavera',3), ('invierno', 3)]
+        solution = [('verano', 3), ('primavera', 3), ('invierno', 3)]
 
-        result, words, set_words = StringsCounter.count_strings(text)
+        result = StringsCounter.count_strings(text)
 
         assert collections.Counter(result) == collections.Counter(solution)
 
-        
 
 if __name__ == '__main__':
     unittest.main()
