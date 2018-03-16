e81315bd22d6e09a1e5af24be6f44c5d161ca8fd
JaviGuilla <javier.guillamon@live.u-tad.com>
2018-03-16 18:56:00 +0100

    Añadida lista de stopwords y metodo para eliminarlas del texto

--- sample/strings_counter.py
+++ sample/strings_counter.py
@@ -15,6 +15,7 @@ class StringsCounter(object):
         # Quitar simbolos de puntuacion
         for i in range(len(text)):
             if not text[i].isdigit() and len(text[i])>1:
+                text[i] = StringsCounter.clear_punctiation_symbols(text[i])
                 text[i] = StringsCounter.clear_stopwords(text[i])
             else:
                 text[i]=""
@@ -42,12 +43,20 @@ class StringsCounter(object):
 
     @staticmethod
     def clear_stopwords(word):
-        stopwords = set(['.', ',', '"', "'", '?', '!', ':',
-                         ';', '(', ')', '[', ']', '{', '}'])
+        stopwords = set(['un', 'una', 'unas', 'unos', 'uno', 'sobre', 'todo', 'también', 'tras', 'otro', 'algún', 'alguno', 'alguna', 'algunos', 'algunas', 'ser', 'es', 'soy', 'eres', 'somos', 'sois', 'estoy', 'esta', 'estamos', 'estais', 'estan', 'como', 'en', 'para', 'atras', 'porque', 'por', 'qué', 'estado', 'estaba', 'ante', 'antes', 'siendo', 'ambos', 'pero', 'por', 'poder', 'puede', 'puedo', 'podemos', 'podeis', 'pueden', 'fui', 'fue', 'fuimos', 'fueron', 'hacer', 'hago', 'hace', 'hacemos', 'haceis', 'hacen', 'cada', 'fin', 'incluso', 'primero', 'desde', 'conseguir', 'consigo', 'consigue', 'consigues', 'conseguimos', 'consiguen', 'ir', 'voy', 'va', 'vamos', 'vais', 'van', 'vaya', 'gueno', 'ha', 'tener', 'tengo', 'tiene', 'tenemos', 'teneis', 'tienen', 'el', 'la', 'lo', 'las', 'los', 'su', 'aqui', 'mio', 'tuyo', 'ellos', 'ellas', 'nos', 'nosotros', 'vosotros', 'vosotras', 'si', 'dentro', 'solo', 'solamente', 'saber', 'sabes', 'sabe', 'sabemos', 'sabeis', 'saben', 'ultimo', 'largo', 'bastante', 'haces', 'muchos', 'aquellos', 'aquellas', 'sus', 'entonces', 'tiempo', 'verdad', 'verdadero', 'verdadera', 'cierto', 'ciertos', 'cierta', 'ciertas', 'intentar', 'intento', 'intenta', 'intentas', 'intentamos', 'intentais', 'intentan', 'dos', 'bajo', 'arriba', 'encima', 'usar', 'uso', 'usas', 'usa', 'usamos', 'usais', 'usan', 'emplear', 'empleo', 'empleas', 'emplean', 'ampleamos', 'empleais', 'valor', 'muy', 'era', 'eras', 'eramos', 'eran', 'modo', 'bien', 'cual', 'cuando', 'donde', 'mientras', 'quien', 'con', 'entre', 'sin', 'trabajo', 'trabajar', 'trabajas', 'trabaja', 'trabajamos', 'trabajais', 'trabajan', 'podria', 'podrias', 'podriamos', 'podrian', 'podriais', 'yo', 'aquel'])
+        print("clear")
         for x in stopwords:
+            print(word)
+            if x==word:
+                word = ""
+                break
+        return word
+    
+    def clear_punctiation_symbols(word):
+        punctuation_symbols = set(['.', ',', '"', "'", '?', '!', ':', ';', '(', ')', '[', ']', '{', '}','<','>','|', ])   
+        for x in punctuation_symbols:
             word = word.replace(x, "")
         return word
-
     @staticmethod
     def count_words(word, text):
         cont = 0
@@ -68,3 +77,4 @@ class StringsCounter(object):
             print("| " + item[0] + " " * (16 - len(item[0]) + space) +
                   "| " + str(item[1]) + " " * (6 - len(str(item[1]))) + "|")
         print ("\n")
+      
