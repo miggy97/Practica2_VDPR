ff0d37bcf9d402f03a2ec976d42b965e51a81893
AlvaroLopez-Jurado <alvaro17lj@gmail.com>
2018-03-18 01:35:49 -0700

    Cambios alvaro

--- tests/test_strings_counter.py
+++ tests/test_strings_counter.py
@@ -7,21 +7,22 @@ class TestStringsCounter(unittest.TestCase):
 
     def test_count_strings(self):
         text = ("verano verano verano")
-        solution = [('verano', 3)]
+        solution=[('verano', 3)]
 
-        result, words, set_words = StringsCounter.count_strings(text)
+        result=StringsCounter.count_strings(text)
 
         assert collections.Counter(result) == collections.Counter(solution)
 
 
     def test_meter_numeros_String(self):
-        text = ("1 2 3 1 2 3 1 2 3 4 alvaro 6")
+        text=("1 2 3 1 2 3 1 2 3 4 alvaro 6")
 
-        solution = [('1', 3), ('2', 3), ('3', 3), ('4', 1), ('alvaro', 1), ('6', 1)]
+        solution=[('1', 3), ('2', 3), ('3', 3), ('4', 1), ('alvaro', 1), ('6', 1)]
 
-        result, words, set_words = StringsCounter.count_strings(text)
+        result= StringsCounter.count_strings(text)
 
         assert collections.Counter(result) == collections.Counter(solution)
+
 
     def test_count_varios_Strings(self):
         text = ("verano verano verano primavera primavera primavera "
@@ -32,6 +33,15 @@ class TestStringsCounter(unittest.TestCase):
 
         assert collections.Counter(result) == collections.Counter(solution)
 
+    #12 tratamiento de siglas
+    def test_una_sigla(self):
+        text = ("C.I.A")
+
+        solution = [('cia', 1)]
+
+        result = StringsCounter.count_strings(text)
+
+        assert collections.Count(result) == collections.Counter(solution)
 
 if __name__ == '__main__':
     unittest.main()
